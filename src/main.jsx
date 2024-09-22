4f809bcc9b1c816410fdb64bf17b0912d04cb339
ElianaA63 <eliana.arias@mi.unc.edu.ar>
2024-09-22 00:09:11 -0300

    SWC-9 moificado y agregado test

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import CrearPartida from "./components/forms/CrearPartida.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}/>
-        <Route path='/CrearPartida' element={<CrearPartida />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
