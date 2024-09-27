b12c7a5f2eeb356cc769aa65c178a84abd946869
Eliana Arias <108479999+ElianaA63@users.noreply.github.com>
2024-09-27 00:27:01 -0300

    Revert "SWC-9 final tests ok"

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-/*manejo de ruteos*/
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import "./index.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./components/login/login.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />}/>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
