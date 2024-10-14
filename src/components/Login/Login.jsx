cdbbe9852fe21e550a918d932f6bbb8825fac00a
ElianaA63 <eliana.arias@mi.unc.edu.ar>
2024-10-14 14:38:49 -0300

    mejoras

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import ParticlesBackground from "./ParticlesBackground";
-import styles from "./Login.module.css";
+import "./Login.css";
 
 const Login = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -43,8 +43,6 @@ const Login = () => {
   }
 };
 
-  
-
   const onSubmit = async (data) => {
     setErrorMessage("");
     try {
@@ -55,51 +53,54 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.root}>
-      <ParticlesBackground /> {/* Agrega el fondo de partículas aquí */}
-      <h2 className={styles.title}>¡Bienvenido a "El Switcher"!</h2>
-      <div className={styles.container}>
-        <div className={styles.formContainer}>
-          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.formGroup}>
-              <input
-                className={styles.input}
-                id="nickname"
-                type="text"
-                placeholder="Nickname"
-                {...register("nickname", {
-                  required: { value: true, message: "El campo no puede estar vacío." },
-                  maxLength: { value: 64, message: "Solo se permiten 64 caracteres." },
-                })}
-              />
-              {errors.nickname && <span className={styles.error}>{errors.nickname.message}</span>}
-            </div>
-            <button type="submit" className={styles.customButton}>
-              Iniciar
-            </button>
-          </form>
-          {errorMessage && <span className={styles.error}>{errorMessage}</span>}
+<div className="root">
+  <ParticlesBackground /> {/* Agrega el fondo de partículas aquí */}
+  <h2 className="title">¡Bienvenido a "El Switcher"!</h2>
+  <div className="container">
+    <div className="formContainer">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="formGroup">
+          <input
+            className="input"
+            id="nickname"
+            type="text"
+            placeholder="Nickname"
+            {...register("nickname", {
+              required: { value: true, message: "El campo no puede estar vacío." },
+              maxLength: { value: 64, message: "Solo se permiten 64 caracteres." },
+            })}
+          />
+          {errors.nickname && <span className="error">{errors.nickname.message}</span>}
         </div>
-        
-        <div className={styles.infoContainer}>
-          <div className={styles.card}>
-            <h2 className={styles['card-title']}>¿Qué es Switcher?</h2>
-            <p className={styles['card-text']}>
-              El Switcher es un juego de mesa de estrategia e ingenio, en el que los jugadores deberán
-              formar figuras determinadas, moviendo las fichas en el tablero, según las tarjetas
-              de movimiento. Cada jugador tendrá un mazo con figuras para ir descartando a medida
-              que se formen en el tablero y evitando que otros jugadores completen sus figuras.
-              Quién primero termine sus cartas de figura será el ganador.
-            </p>
-            <h2 className={styles['card-title']}>¿Cómo jugar?</h2>
-            <a href="https://maldon.com.ar/wp-content/uploads/2018/09/Reglamento-el-Switcher-2018.pdf" target="_blank" rel="noopener noreferrer">
-              <button className={styles['custom-button']}>Descargar reglas</button>
-            </a>
-          </div>
-        </div>
+        <button type="submit" className="customButton">
+          Iniciar
+        </button>
+      </form>
+      {errorMessage && <span className="error">{errorMessage}</span>}
+    </div>
+    
+    <div className="infoContainer">
+      <div className="card">
+        <h2 className="card-title">¿Qué es Switcher?</h2>
+        <p className="card-text">
+          El Switcher es un juego de mesa de estrategia e ingenio, en el que los jugadores deberán
+          formar figuras determinadas, moviendo las fichas en el tablero, según las tarjetas
+          de movimiento. Cada jugador tendrá un mazo con figuras para ir descartando a medida
+          que se formen en el tablero y evitando que otros jugadores completen sus figuras.
+          Quién primero termine sus cartas de figura será el ganador.
+        </p>
+        <h2 className="card-title">¿Cómo jugar?</h2>
+        <a href="https://maldon.com.ar/wp-content/uploads/2018/09/Reglamento-el-Switcher-2018.pdf" target="_blank" rel="noopener noreferrer">
+          <button className="custom-button">Descargar reglas</button>
+        </a>
       </div>
     </div>
+  </div>
+</div>
   );
 };
 
 export default Login;
+
+
+
