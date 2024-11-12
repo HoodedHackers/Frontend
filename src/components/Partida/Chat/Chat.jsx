5f7b2f1748e6a7a73c98f57c1335208c50d47e51
Camilo649 <camilo.gutierrez.649@mi.unc.edu.ar>
2024-11-12 05:30:11 -0300

    manejo de logs con regex

--- src/components/Partida/Chat/Chat.jsx
+++ src/components/Partida/Chat/Chat.jsx
@@ -8,74 +8,63 @@
     const { wsCRef } = useContext(WebSocketContext);
     const game_id = sessionStorage.getItem('partida_id');
     const player_id = sessionStorage.getItem('player_id'); // Asumiendo que tienes el player_id en sessionStorage
-  
-    // Configurar WebSocket y escuchar mensajes
-    useEffect(() => {
-      if (wsCRef.current && wsCRef.current.readyState !== WebSocket.CLOSED) {
-        return;
-      }
-  
-      // Establecer conexión WebSocket
-      try {
-        wsCRef.current = new WebSocket(
-          `ws://127.0.0.1:8000/ws/lobby/${game_id}/chat?player_id=${player_id}`
-        );
-  
-        wsCRef.current.onopen = () => {
-          console.log("Conexión con el WebSocket del Chat abierta");
-        };
-  
-        wsCRef.current.onmessage = (event) => {
-          const mensaje = JSON.parse(event.data);
-          if (mensaje.message) {
-            if (mensaje.message.includes('descartado')) {
-              setMensajes(prevMensajes => [
-                { usuario: 'Sistema', mensaje: mensaje.message, tipo: 'descarte' },
-                ...prevMensajes,
-              ]);
-            }
-            else if (mensaje.message.includes('bloqueado')) {
-              setMensajes(prevMensajes => [
-                { usuario: 'Sistema', mensaje: mensaje.message, tipo: 'bloqueo' },
-                ...prevMensajes,
-              ]);
-            }
-            else if (mensaje.message.includes('turno')) {
-              setMensajes(prevMensajes => [
-                { usuario: 'Sistema', mensaje: mensaje.message, tipo: 'turno' },
-                ...prevMensajes,
-              ]);
-            }
-            else if (mensaje.message.includes('abandonado')) {
-              setMensajes(prevMensajes => [
-                { usuario: 'Sistema', mensaje: mensaje.message, tipo: 'abandono' },
-                ...prevMensajes,
-              ]);
-            }
-            else {
-              setMensajes(prevMensajes => [
-                { usuario: mensaje.name, mensaje: mensaje.message, tipo: '' },
-                ...prevMensajes,
-              ]);
-            }
-          }
-        };
-  
-        wsCRef.current.onerror = (error) => {
-          console.error("Error en el WebSocket del Chat: ", error);
-        };
-  
-        wsCRef.current.onclose = () => {
-          console.log("Conexión WebSocket cerrada");
-        };
-      } catch (error) {
-        console.error("Error al crear WebSocket: ", error);
-        if (wsCRef.current) {
-          wsCRef.current.close();
+    
+  // Configurar WebSocket y escuchar mensajes
+  useEffect(() => {
+    if (wsCRef.current && wsCRef.current.readyState !== WebSocket.CLOSED) {
+      return;
+    }
+
+    try {
+      wsCRef.current = new WebSocket(
+        `ws://127.0.0.1:8000/ws/lobby/${game_id}/chat?player_id=${player_id}`
+      );
+
+      wsCRef.current.onopen = () => {
+        console.log("Conexión con el WebSocket del Chat abierta");
+      };
+
+      wsCRef.current.onmessage = (event) => {
+        const mensaje = JSON.parse(event.data);
+
+        if (mensaje.message) {
+          // Mapeo de patrones de regex a los tipos de mensaje
+          const tiposMensaje = {
+            descarte: /ha descartado una carta/,
+            bloqueo: /ha bloqueado una carta/,
+            turno: /^turno de\b/,
+            abandono: /ha abandonado la partida/
+          };
+
+          // Identificar tipo de mensaje usando regex
+          const tipo = Object.entries(tiposMensaje).find(([_, regex]) =>
+            regex.test(mensaje.message)
+          )?.[0] || ''; // Por defecto vacío si no coincide con ningún tipo
+
+          // Agregar el mensaje con el tipo identificado
+          setMensajes(prevMensajes => [
+            { usuario: tipo ? 'Sistema' : mensaje.name, mensaje: mensaje.message, tipo },
+            ...prevMensajes,
+          ]);
         }
+      };
+
+      wsCRef.current.onerror = (error) => {
+        console.error("Error en el WebSocket del Chat: ", error);
+      };
+
+      wsCRef.current.onclose = () => {
+        console.log("Conexión WebSocket cerrada");
+      };
+    } catch (error) {
+      console.error("Error al crear WebSocket: ", error);
+      if (wsCRef.current) {
+        wsCRef.current.close();
       }
-    }, [wsCRef.current, game_id, player_id]);
-  
+    }
+  }, [wsCRef.current, game_id, player_id]);
+
+
     // Enviar mensaje al WebSocket
     const enviarMensaje = () => {
       if (nuevoMensaje.trim() !== '') {
